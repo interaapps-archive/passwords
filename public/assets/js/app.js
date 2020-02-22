d09747e7c8bb09fbd7663e5a85e9df6328b85f79
julianfun123 <julianfun123@yahoo.com>
2020-02-22 22:14:09 +0100

    Changed the design a bit

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,8 @@
-const root = "https://passwords.interaapps.de/";
+let root = "/";
+
+if (typeof chrome.tabs != 'undefined')
+    root = "https://passwords.interaapps.de/";
+
 let password;
 
 function randomString(length, characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') {
@@ -97,6 +101,8 @@ function loadList(){
             if (decrypt(pw.name) != "%$test$%")
             $("#passwords").append(
                 $n("div").addClass("password").append(
+                    $n("img").addClass("password_icon").css("display", "none").attr("src", decrypt(pw.icon))
+                ).append(
                     $n("span").addClass("password_name").text(decrypt(pw.name))
                 ).append(
                     $n("span").addClass("password_password").text(decrypt(pw.password).replace(/./g, '*'))
@@ -166,6 +172,10 @@ function loadList(){
 
                     pwAlert.open();
             }));
+
+            $("img").on("load", function(e){
+                $(e.target).css("display","inline");
+            });
         }
     }).send();
     
@@ -180,19 +190,21 @@ const ready = function(){
             var masterPassAlert = new Alert({
                 canexit: false,
                 closebtn: false,
-                title: "Set Master Password"
+                title: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Set Master Password"
             });
         
-            masterPassAlert.addHtml('<input id="masterpass" type="password" class="flatInput">');
-        
-            masterPassAlert.addButton("  Set Masterpassword", function() {
+            masterPassAlert.addHtml('<img id="alert_preview" src="assets/images/safe.svg"><input id="masterpass" type="password" class="flatInput"><a id="random_password">Random password</a><br>Please select a secure password and keep it!<br>If you want to reset your password after that you\'ll lose your password.<br>Write it down or save it into a file.');
+            $("#random_password").click(function(){
+                $("#alert_preview").val(randomString(25));
+            });
+            masterPassAlert.addButton("Set Masterpassword", function() {
                 password = $("#masterpass").val();
                 addPassword("%$test$%", "test","","","","",function(){
                     showSnackBar("Done");
                     masterPassAlert.close();
                     ready();
                 });
-            }, "add");
+            });
 
             masterPassAlert.open();
         
@@ -200,11 +212,42 @@ const ready = function(){
             var masterPassAlert = new Alert({
                 canexit: false,
                 closebtn: false,
-                title: "Give me your Master Password"
+                title: "&nbsp;&nbsp;Give me your Master Password"
             });
         
-            masterPassAlert.addHtml('<input id="masterpass" type="password" class="flatInput">');
+            masterPassAlert.addHtml('<img id="alert_preview" src="assets/images/authentication.svg"><input id="masterpass" type="password" class="flatInput"><br><a id="forgot_password">Forgot password?</a>');
         
+            $("#forgot_password").click(function(){
+                var newPasswordConfirmation = new Alert({
+                    canexit: true,
+                    closebtn: true,
+                    title: "Are you sure?"
+                });
+
+                newPasswordConfirmation.addHtml('<img id="alert_preview" src="assets/images/throw_down.svg">Do you really want to set a new password? This will reset all of your passwords!');
+                newPasswordConfirmation.addButton("Confirm", function(){
+                    for (obj in parsed.list) {
+                        console.log("Hi");
+                        const pw = parsed.list[obj];
+                        Cajax.delete(root+"passwords/"+pw.id+"/remove").then(function(){
+                            console.log(parsed.list.length-1);
+                            console.log(obj);
+                            if ((parsed.list.length-1) == obj) {
+                                ready();
+                                newPasswordConfirmation.close();
+                                masterPassAlert.close();
+                            }
+                        }).send();
+                    }
+                });
+                newPasswordConfirmation.addButton("Close", function(){
+                    newPasswordConfirmation.close();
+                });
+                newPasswordConfirmation.open();
+
+
+            });
+
             masterPassAlert.addButton("Go!", function() {
                 password = $("#masterpass").val();
                 console.log(parsed.list[0].password);
