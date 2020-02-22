f9db943eff34afdee5554c3a139f68ec0d07a749
julianfun123 <julianfun123@yahoo.com>
2020-02-22 22:26:02 +0100

    Changed the design a bit

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,6 @@
 let root = "/";
 
-if (chrome != null && typeof chrome.tabs != 'undefined')
+if (typeof chrome != 'undefined' && typeof chrome.tabs != 'undefined')
     root = "https://passwords.interaapps.de/";
 
 let password;
